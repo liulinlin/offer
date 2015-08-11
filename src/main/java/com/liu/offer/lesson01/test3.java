4a2395af1d99f7a166214fa16dddf7785409a361
刘林林 <553656977@qq.com>
2015-08-11 22:09:43 +0800

    剑指offer

---
+++ src/main/java/com/liu/offer/lesson01/test3.java
@@ -0,0 +1,28 @@
+package com.liu.offer.lesson01;
+
+/**
+ * Hello world!
+ *
+ */
+public class test3 
+{
+	public boolean Find(int [][] array ,int rows ,int colums,int number){
+		boolean found = false;
+		while(array != null && rows >0 && colums >0){
+			int row = 0;
+			int colum = colums-1;
+			while(row < rows && colum >= 0){
+				if(array[row][colum] == number){
+					found = true;
+					break;
+				}
+				if(array[row][colum] > number){
+					row++;
+				}else{
+					colum--;
+				}
+			}
+		}
+		return found;
+	}
+}
---
+++ src/main/java/com/liu/offer/lesson02/Node.java
@@ -0,0 +1,7 @@
+package com.liu.offer.lesson02;
+
+public class Node {
+	Node left = null;
+	Node right = null;
+	char value;
+}
