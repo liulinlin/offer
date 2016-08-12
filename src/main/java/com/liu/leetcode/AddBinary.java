b9d6d349dcffaf24a89fcd1a4ead3844b60e838c
553656977@qq.com <553656977@qq.com>
2016-08-12 18:43:35 +0800

    leetCode

--- src/main/java/com/liu/leetcode/AddBinary.java
+++ src/main/java/com/liu/leetcode/AddBinary.java
@@ -18,6 +18,7 @@ public class AddBinary {
         System.out.println(addBinary(a,b));
     }
 
+    // MySolution
     public static String addBinary(String a, String b) {
         if (a == null || a.trim() == ""){
             return  b;
@@ -77,5 +78,69 @@ public class AddBinary {
             retuString = retuString.substring(1);
         }
         return  retuString;
+    }
+
+    // other's solution
+    public String addBinary1(String a, String b) {
+        String temp="";
+        if(a.length()<b.length()){
+            temp=a;
+            a=b;
+            b=temp;
+        }
+
+        char[] arA=a.toCharArray();
+        char[] arB=b.toCharArray();
+        char[] arC=new char[a.length()+1];
+
+        int i=a.length()-1;
+        int j=b.length()-1;
+        int carry=0;
+        int tempSum=0;
+        while(i>=0){
+            if(j>=0){
+                tempSum=arA[i]-'0'+arB[j]-'0'+carry;
+                --j;
+            }
+            else{
+                tempSum=arA[i]-'0'+carry;
+            }
+            carry=tempSum/2;
+            arC[i+1]=(char)(tempSum%2+48);
+            --i;
+        }
+        arC[i+1]='1';
+
+        if(carry>0){
+            return String.valueOf(arC);
+        }
+        else{
+            return String.valueOf(arC).substring(1,a.length()+1);
+        }
+    }
+    public String addBinary2(String a, String b) {
+        StringBuilder result = new StringBuilder();
+        int sum = 0, carry = 0;
+        int i = a.length() - 1, j = b.length() - 1;
+
+        // start from the end
+        while(i >= 0 || j >= 0){
+            // take the bits
+            int bit1 = (i >= 0) ? Integer.parseInt(String.valueOf(a.charAt(i))) : 0;
+            int bit2 = (j >= 0) ? Integer.parseInt(String.valueOf(b.charAt(j))) : 0;
+
+            // calculate the sum and the carry
+            sum = carry ^ bit1 ^ bit2;
+            carry = (carry & bit1) | (carry & bit2) | (bit1 & bit2);
+
+            result.append(String.valueOf(sum));
+            i--;
+            j--;
+        }
+
+        if(carry > 0)
+            result.append(String.valueOf(carry));
+
+        return result.reverse().toString();
     }
 }
