4fe9d9f9d4a21133094dd11c47c830afc194db35
Administrator <553656977@qq.com>
2016-09-05 18:55:08 +0800

    整理代码

---
+++ src/main/java/com/liu/offer/JD/_20160905/Main.java
@@ -0,0 +1,9 @@
+package com.liu.offer.JD._20160905;
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+
+    }
+}
