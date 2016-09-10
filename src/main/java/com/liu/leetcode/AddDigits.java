33f4f4a7c9c893fe12c5077bb4e5252fe5bd572a
Administrator <553656977@qq.com>
2016-09-10 18:55:12 +0800

    添加注释

--- src/main/java/com/liu/leetcode/AddDigits.java
+++ src/main/java/com/liu/leetcode/AddDigits.java
@@ -1,13 +1,22 @@
 package com.liu.leetcode;
 
 public class AddDigits {
-	public static int addDigits(int num) {
-		if (num <= 0) {
-			return 0;
-		}
-		if (num >= Integer.MAX_VALUE) {
-			return 0;
-		}
-		return num - 9 * ((num-1) /9 );
-	}
+    public static void main(String[] args) {
+        System.out.println(addDigits(110111));
+    }
+
+    /**
+     * 记录有多少个1
+     * @param num 数
+     * @return  个数
+     */
+    public static int addDigits(int num) {
+        if (num <= 0) {
+            return 0;
+        }
+        if (num >= Integer.MAX_VALUE) {
+            return 0;
+        }
+        return num - 9 * ((num - 1) / 9);
+    }
 }
