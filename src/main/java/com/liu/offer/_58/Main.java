15243989dee93eaf7ddf69a84d6ab56042d0db64
liulinlin <553656977@qq.com>
2017-03-23 22:00:22 +0800

    更新

--- src/main/java/com/liu/offer/_58/Main.java
+++ src/main/java/com/liu/offer/_58/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 /**
  * Created by liulinlin on 2017/3/23.
+ * 平衡数问题：1221,100,1236 1236 可以拆成1*2*3==6  1*2==2*1 1*0 == 0
  */
 public class Main {
     public static void main(String[] args) {
@@ -15,12 +16,23 @@ public class Main {
 
     public static void getValue(int data) {
         String s = String.valueOf(data);
-        int R = 1;
+        int result = 1;
+        int countOf0 = 0;
         for (int i = 0; i < s.length(); i++) {
-            R = R * (s.charAt(i) - '0');
+            int l = s.charAt(i) - '0';
+            if (l == 0) countOf0++;
+            result = result * l;
         }
-        int r = (int) Math.sqrt((double) R);
-        if (r * r == R) System.out.println("YES");
+        if (countOf0 == 1) {
+            System.out.println("NO");
+            return;
+        }
+        if (countOf0 > 1) {
+            System.out.println("YES");
+            return;
+        }
+        int r = (int) Math.sqrt((double) result);
+        if (r * r == result) System.out.println("YES");
         else System.out.println("NO");
     }
 }
